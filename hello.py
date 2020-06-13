0ff4fa27cc1556deb8d50ab34fa0ba7593ab24c4
zhangneng <zhntrust@163.com>
2020-06-13 14:36:51 +0800

    add auther

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@
 @Author: zhangneng
 @Date: 2020-06-13 12:41:36
 @LastEditors: zhagneng
-@LastEditTime: 2020-06-13 14:35:36
+@LastEditTime: 2020-06-13 14:36:35
 @Description: file content
 '''
 
 init hello
+zhangneng
