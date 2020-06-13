4687a190e31c3cb7207b42cf4a16f0d8dfe45561
zhangneng-old <zhntrust@gmail.com>
2020-06-13 15:10:47 +0800

    flash hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 '''
 @Author: zhangneng
 @Date: 2020-06-13 12:41:36
-@LastEditors: zhagneng
-<<<<<<< HEAD
-@LastEditTime: 2020-06-13 15:04:04
-=======
-@LastEditTime: 2020-06-13 14:41:09
->>>>>>> add email
+@LastEditors: Please set LastEditors
+@LastEditTime: 2020-06-13 15:09:51
 @Description: file content
 '''
 
