99ad0bea1bb693f0c1ec89114e78d618beb454fc
zhangneng <zhntrust@163.com>
2020-06-13 14:36:15 +0800

    init hello

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@
 @Author: zhangneng
 @Date: 2020-06-13 12:41:36
 @LastEditors: zhagneng
-@LastEditTime: 2020-06-13 12:41:37
+@LastEditTime: 2020-06-13 14:35:36
 @Description: file content
 '''
+
+init hello
