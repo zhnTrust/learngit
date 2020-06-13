ca56fbf863c373f9964ed759cd06673c9d53ed1e
zhangneng <zhntrust@163.com>
2020-06-13 14:41:23 +0800

    add email

--- hello.py
+++ hello.py
@@ -2,7 +2,11 @@
 @Author: zhangneng
 @Date: 2020-06-13 12:41:36
 @LastEditors: zhagneng
+<<<<<<< HEAD
 @LastEditTime: 2020-06-13 15:04:04
+=======
+@LastEditTime: 2020-06-13 14:41:09
+>>>>>>> add email
 @Description: file content
 '''
 
@@ -10,3 +14,4 @@ init hello
 zhangneng
 add email
 add word
+add email
