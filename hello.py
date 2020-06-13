c328481c314f517a091de59522f5eb72145b69b6
zhangneng <zhntrust@163.com>
2020-06-13 14:40:24 +0800

    add world

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 '''
 @Author: zhangneng
 @Date: 2020-06-13 12:41:36
-@LastEditors: Please set LastEditors
-@LastEditTime: 2020-06-13 14:45:28
+@LastEditors: zhagneng
+@LastEditTime: 2020-06-13 15:04:04
 @Description: file content
 '''
 
 init hello
 zhangneng
-
-add addr
+add email
+add word
